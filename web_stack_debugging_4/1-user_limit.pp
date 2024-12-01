2df9f14ef25bb0dfe204f5daa810e8375901ef3d
DubaKanu <j.kanu@alustudent.com>
2024-12-01 17:24:16 +0000

    User limit

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,5 @@
-exec { 'change-os-configuration-for-holberton-user':
-  command => '/bin/echo "holberton soft nofile 4096\nholberton hard nofile 65536" >> /etc/security/limits.conf && /sbin/sysctl -p',
+# This Puppet script adjusts the OS configuration to increase file descriptor limits for the holberton user.
+exec { 'adjust-file-descriptor-limits':
+  command => '/bin/echo "holberton soft nofile 4096\nholberton hard nofile 65536" >> /etc/security/limits.conf',
   path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
 }
